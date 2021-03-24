6590e55b2eb441ef18522b766723c0617bd49fe5
GryGabriel <79148479+GryGabriel@users.noreply.github.com>
2021-03-24 22:14:07 +0100

    Add files via upload

---
+++ hazi06/Konyv.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace konyvNevter
+{
+    class Konyv
+    {
+        private string cim;
+        private string szerzo;
+        private readonly int megjelenesEve = DateTime.Now.Year;
+        private int ar;
+        private int oldalszam;
+        private static string kiado = "Móra";
+
+        public int Oldalszam
+        {
+            get { return oldalszam; }
+            set { oldalszam = value; }
+        }
+
+
+        public Konyv(string cim, string szerzo, int oldalszam, int ar)
+        {
+            this.cim = cim;
+            this.szerzo = szerzo;
+            this.ar = ar;
+            this.oldalszam = oldalszam;
+        }
+
+        public Konyv(string cim, string szerzo, int oldalszam) : this(cim, szerzo, oldalszam, 2500)
+        {
+
+        }
+
+        public int Ar
+        {
+            get { return ar; }
+            set { ar = value; }
+        }
+
+        public string Szerzo
+        {
+            get { return szerzo; }
+            set { szerzo = value; }
+        }
+
+        public string Cim
+        {
+            get { return cim; }
+            set { cim = value; }
+        }
+
+        public void ArNoveles(int szazalek)
+        {
+            ar += ar * szazalek / 100;
+        }
+
+        public string Osszefuz()
+        {
+            return $"A könyv címe: {cim},\nSzerzője: {szerzo},\nA kiadó: {kiado},\nMegjelenésének éve: {megjelenesEve},\nOldalszáma: {oldalszam},\nÁra: {ar}\n ";
+        }
+
+        public override string ToString()
+        {
+            return $"A könyv címe: { cim},\nSzerzője: { szerzo},\nA kiadó: {kiado},\nMegjelenésének éve: { megjelenesEve},\nOldalszáma: {oldalszam},\nÁra: { ar}\n";
+        }
+
+        public static Konyv HosszabbKonyv(Konyv konyv1, Konyv konyv2)
+        {
+            if(konyv1.oldalszam > konyv2.oldalszam)
+            {
+                return konyv1;
+            }
+            else
+            {
+                return konyv2;
+            }
+        }
+
+        public bool ParosE()
+        {
+            if(oldalszam%2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ hazi06/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using konyvNevter;
+
+namespace hazi06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Konyv konyv1 = new Konyv("A kendőzetlen igazság", "Bakó Csaba", 600, 4990);
+            Console.WriteLine(konyv1.ToString());
+            Console.WriteLine(konyv1.Osszefuz());
+
+            Konyv konyv2 = new Konyv("A két lotti", "Erich Kästner", 456);
+            Console.WriteLine(konyv2.ToString());
+
+            konyv1.ArNoveles(20);
+            Console.WriteLine(konyv1.ToString());
+
+            Console.WriteLine(Konyv.HosszabbKonyv(konyv1, konyv2));
+
+            Console.WriteLine(konyv1.ParosE());
+
+            Console.WriteLine("Hány könyvet olvasson be?");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Konyv[] konyvek = new Konyv[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Add meg a könyv: Címét, Szerzőjét, Oldalszámát, Árát");
+                konyvek[i] = new Konyv(Console.ReadLine(), Console.ReadLine(), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+            }
+
+            int maxindex = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if(konyvek[i].Oldalszam > konyvek[maxindex].Oldalszam)
+                {
+                    maxindex = i;
+                }
+            }
+            Console.WriteLine($"A leghosszabb könyv adatai:\n {konyvek[maxindex]}");
+
+            int leghosszabb=0;
+            for (int i = 0; i < n; i++)
+            {
+                if(konyvek[i].Oldalszam%2 == 0)
+                {
+                    leghosszabb = i;
+                    break;
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if(konyvek[i].Oldalszam%2 == 0)
+                {
+                    if (konyvek[i].Oldalszam > konyvek[leghosszabb].Oldalszam)
+                    {
+                        leghosszabb = i;
+                    }
+                }
+            }
+            Console.WriteLine($"A leghosszabb páros oldalszámú könyv adatai:\n {konyvek[leghosszabb]}");
+
+            for (int i = 0; i < n; i++)
+            {
+                int elofordulas = 1;
+                for (int j = 0; j < n; j++)
+                {
+                    if(konyvek[i].Szerzo == konyvek[j].Szerzo && i != j)
+                    {
+                        elofordulas++;
+                    }
+                }
+                Console.WriteLine($"{konyvek[i].Szerzo}-nak/-nek {elofordulas} könyve jelent meg.");
+            }
+        }
+    }
+}
